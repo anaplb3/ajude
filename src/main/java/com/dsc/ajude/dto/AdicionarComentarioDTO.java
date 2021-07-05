92266e763c9a78752bc611ca9b803ff958e34e22
anaplb3 <ana.paula@dcx.ufpb.br>
2021-07-04 23:43:32 -0300

    classe esquecida no meio do caminho

--- src/main/java/com/dsc/ajude/dto/AdicionarComentarioDTO.java
+++ src/main/java/com/dsc/ajude/dto/AdicionarComentarioDTO.java
@@ -2,15 +2,18 @@ package com.dsc.ajude.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class AdicionarComentarioDTO {
-	
+
+	@NotNull(message = "Necessário informar a Campanha.")
 	private Long idCampanha;
-	
-	private Long idComentario;
-	
+
+	@NotNull(message = "Corpo da mensagem não pode ser vazia.")
 	private String corpoDaMensagem;
-	
+
+	@NotNull(message = "Email do Usuário é obrigatório.")
 	private String emailDoUsuario;
 
 
